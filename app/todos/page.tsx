e22383b27f72f5478910d5a2c2aba0bc1fb9bb50
AMD <michojekunle1@gmail.com>
2024-03-19 20:07:25 +0100

    changes

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,9 +1,15 @@
 import {getData} from "@/actions/todoActions";
 import Todos from "@/components/custom/Todos";
-import { getSession } from 'next-auth/react';
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-
+  const session = await getServerSession(authOptions)
   const data = await getData();
+  if (!session) {
+    redirect('/api/auth/signin')
+    return;
+  }
   return <Todos todos={data} />;
 }
