080245d1fb7e74c80ec018c0ba05b4f5af50be04
AMD <michojekunle1@gmail.com>
2024-03-19 20:18:31 +0100

    made changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,5 @@ import NextAuth from "next-auth/next";
 import { authOptions } from "./options";
 
 
-export const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST };
